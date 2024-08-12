a2bc8d5c5c55d2d5da3e01cece0f0671d56d147e
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-11 23:48:10 -0500

    got a functional test to pass

---
+++ Mocks/mockBoard.go
@@ -0,0 +1,36 @@
+package Mocks
+
+import (
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"kanban-go/models"
+)
+
+type MockBoardService struct {
+	mock.Mock
+}
+
+func (m *MockBoardService) CreateBoard(BoardDto models.BoardDto) error {
+	args := m.Called(BoardDto)
+	return args.Error(0)
+}
+
+func (m *MockBoardService) GetBoards() ([]models.Board, error) {
+	args := m.Called()
+	return args.Get(0).([]models.Board), args.Error(1)
+}
+
+func (m *MockBoardService) GetBoardById(id primitive.ObjectID) (models.Board, error) {
+	args := m.Called(id)
+	return args.Get(0).(models.Board), args.Error(1)
+}
+
+func (m *MockBoardService) UpdateBoard(Board models.Board) error {
+	args := m.Called(Board)
+	return args.Error(0)
+}
+
+func (m *MockBoardService) DeleteBoard(id primitive.ObjectID) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
---
+++ routes/boardRoutes_test.go
@@ -0,0 +1,74 @@
+package routes
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"kanban-go/Mocks"
+	"kanban-go/models"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestBoardRoutes_MethodCalling(t *testing.T) {
+	router := gin.Default()
+	mockService := new(Mocks.MockBoardService)
+	BoardRoutes(router, mockService)
+
+	boardDto := models.BoardDto{Name: "Test Board"}
+	id := primitive.NewObjectID()
+	createdTime := time.Now()
+	board := models.Board{
+		ID:        &id,
+		Name:      "TestBoard",
+		CreatedAt: createdTime,
+		Columns:   nil,
+	}
+
+	// expectations
+	mockService.On("CreateBoard", boardDto).Return(nil)
+	mockService.On("GetBoards").Return([]models.Board{board}, nil)
+	mockService.On("GetBoardById", mock.Anything).Return(board, nil)
+	mockService.On("UpdateBoard", mock.Anything).Return(nil)
+	mockService.On("DeleteBoard", mock.Anything).Return(nil)
+
+	req, _ := http.NewRequest("GET", "/api/v1/boards", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	boardId := board.ID.Hex()
+	req, _ = http.NewRequest("GET", "/api/v1/board/"+boardId, nil)
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	boardDtoJSON, _ := json.Marshal(boardDto)
+	req, _ = http.NewRequest("POST", "/api/v1/board", bytes.NewBuffer(boardDtoJSON))
+	req.Header.Set("Content-Type", "application/json")
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	boardJSON, _ := json.Marshal(board)
+	req, _ = http.NewRequest("PUT", "/api/v1/board", bytes.NewBuffer(boardJSON))
+	req.Header.Set("Content-Type", "application/json")
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	reqJSON, _ := json.Marshal(models.IdRequest{ID: boardId})
+	req, _ = http.NewRequest("DELETE", "/api/v1/board", bytes.NewBuffer(reqJSON))
+	req.Header.Set("Content-Type", "application/json")
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	mockService.AssertExpectations(t)
+
+}
