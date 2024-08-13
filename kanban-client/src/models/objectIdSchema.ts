818a9c4d722fd2e272772cd14c5876a2dc8d6d2a
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-13 14:37:09 -0500

    restarting client side

--- kanban-client/src/models/objectIdSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { ObjectId } from "bson";
-
-export const objectIdSchema = z.custom<ObjectId>((value) => {
-    return ObjectId.isValid(value)
-}, {
-    message: "Invalid MongoDB ObjectId"
-})
