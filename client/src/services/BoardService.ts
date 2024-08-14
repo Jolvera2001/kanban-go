bab215fa19bbb3cbcf03e7347cecaafdc17d3868
Jolvera2001 <jolvera6@stedwards.edu>
2024-08-14 16:32:26 -0500

    finished createBoard service method

--- client/src/services/BoardService.ts
+++ client/src/services/BoardService.ts
@@ -1,11 +1,37 @@
-import { boardType } from "@/models/boardType";
+import {boardSchema, boardType} from "@/models/boardType";
 import { IBoardService } from "@/services/iBoardService.ts";
 import { ObjectId } from "bson";
 
 export class BoardService implements IBoardService {
+    private readonly baseApiUrl : string;
 
-    createBoard(boardName: string): Promise<boardType[]> {
-        throw new Error("Method not implemented.");
+    constructor() {
+        this.baseApiUrl = "http://localhost:8080/api/v1/"
+    }
+
+    async createBoard(boardName: string): Promise<boardType> {
+        const data = { Name: boardName}
+
+        try {
+            const response = await fetch(`${this.baseApiUrl}/board`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok")
+            }
+
+            const json = await response.json();
+            return boardSchema.parse(json);
+
+        } catch (e) {
+            console.log(e);
+            return {} as boardType;
+        }
     }
     getBoards(): Promise<boardType[]> {
         throw new Error("Method not implemented.");
@@ -19,5 +45,10 @@ export class BoardService implements IBoardService {
     deleteBoard(id: ObjectId): string {
         throw new Error("Method not implemented.");
     }
+}
 
+export class BoardServiceFactory {
+    public static createService(): IBoardService {
+        return new BoardService();
+    }
 }
--- client/src/services/iBoardService.ts
+++ client/src/services/iBoardService.ts
@@ -2,7 +2,7 @@ import {boardType} from "@/models/boardType.ts";
 import {ObjectId} from "bson";
 
 export interface IBoardService {
-    createBoard(boardName: string): Promise<boardType[]>;
+    createBoard(boardName: string): Promise<boardType>;
     getBoards(): Promise<boardType[]>;
     getBoardById(id: ObjectId): Promise<boardType>;
     updateBoard(updatedBoard: boardType): string;
